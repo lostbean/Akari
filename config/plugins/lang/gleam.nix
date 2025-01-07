157c4832a1350f3e1ec0561680392a3b642a9de5
lostbean <talktoedgar@gmail.com>
2025-01-07 17:33:01 -0300

    setup gleam formatter

--- config/plugins/lang/gleam.nix
+++ config/plugins/lang/gleam.nix
@@ -1,5 +1,19 @@
+{ pkgs, ... }:
 {
-  plugins.lsp.servers.gleam = {
-    enable = true;
+  plugins = {
+    lsp.servers.gleam.enable = true;
+
+    conform-nvim.settings = {
+      formatters_by_ft = {
+        gleam = [ "gleam" ];
+      };
+
+      formatters = {
+        gleam = {
+          command = "${pkgs.gleam}/bin/gleam";
+        };
+      };
+    };
   };
+
 }
