8fc792bd84cfad52d40db7ab79d61305fde05f65
lostbean <talktoedgar@gmail.com>
2025-01-09 10:12:34 -0300

    override spider key description

--- config/plugins/editor/spider.nix
+++ config/plugins/editor/spider.nix
@@ -1,13 +1,34 @@
 {
-  plugins.spider = {
-    enable = true;
-    keymaps = {
-      motions = {
-        w = "w";
-        e = "e";
-        b = "b";
-        ge = "ge";
+  plugins = {
+    spider = {
+      enable = true;
+      keymaps = {
+        motions = {
+          w = "w";
+          e = "e";
+          b = "b";
+          ge = "ge";
+        };
       };
     };
+
+    which-key.settings.spec = [
+      {
+        __unkeyed = "w";
+        desc = "Next word (spider)";
+      }
+      {
+        __unkeyed = "e";
+        desc = "Next end of word (spider)";
+      }
+      {
+        __unkeyed = "b";
+        desc = "Prev word (spider)";
+      }
+      {
+        __unkeyed = "ge";
+        desc = "Prev end of word (spider)";
+      }
+    ];
   };
 }
