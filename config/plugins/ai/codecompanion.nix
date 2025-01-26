3d9dd51818bcce6dd9a20b265a14a59eea7a103a
lostbean <talktoedgar@gmail.com>
2025-01-26 17:01:50 -0300

    add new prompt for continuing with the rest of the implementation

--- config/plugins/ai/codecompanion.nix
+++ config/plugins/ai/codecompanion.nix
@@ -52,6 +52,84 @@
             }
           ];
         };
+
+        "Continue Implementation" = {
+          strategy = "chat";
+          description = "Continue Implementation based on the current git diff";
+          opts = {
+            index = 5;
+            is_default = true;
+            is_slash_cmd = false;
+            auto_submit = false;
+            user_prompt = false;
+            stop_context_insertion = true;
+          };
+          prompts = [
+            {
+              role = "user";
+              content.__raw = ''
+                function(context)
+                  local diagnostics = require("codecompanion.helpers.actions").get_diagnostics(
+                    context.start_line,
+                    context.end_line,
+                    context.bufnr
+                  )
+
+                  local concatenated_diagnostics = ""
+                  for i, diagnostic in ipairs(diagnostics) do
+                    concatenated_diagnostics = concatenated_diagnostics
+                      .. i
+                      .. ". Issue "
+                      .. i
+                      .. "\n  - Location: Line "
+                      .. diagnostic.line_number
+                      .. "\n  - Buffer: "
+                      .. context.bufnr
+                      .. "\n  - Severity: "
+                      .. diagnostic.severity
+                      .. "\n  - Message: "
+                      .. diagnostic.message
+                      .. "\n"
+                  end
+
+                  local buf_utils = require("codecompanion.utils.buffers")
+
+
+                  return string.format(
+                    [[
+                I want you to act as a senior %s developer. Your goal is to continue the propagating the modifications from where I stopped.
+                Here is diff of what I have done so far:
+                ```diff
+                %s
+                ```
+
+                You may also find it useful to see the warnings/errors from the LSP:
+                %s
+
+
+                And this is the code we are working next:
+                ```%s
+                %s
+                ```
+
+                Now, you update the following code to make it fully functional and up to date with previous modifications. Provade all code modifications as diff format.
+
+                    ]],
+                    context.filetype,
+                    vim.fn.system("git diff --no-ext-diff HEAD"),
+                    concatenated_diagnostics,
+                    context.filetype,
+                    buf_utils.get_content(context.bufnr)
+                  )
+                end
+              '';
+              opts = {
+                visible = false;
+                tag = "system_tag";
+              };
+            }
+          ];
+        };
       };
       opts = {
         log_level = "TRACE";
